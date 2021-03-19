dff225d7401a9d8819fde1c3cac2a14e2ff024b7
tieshengdetie <123844231@qq.com>
2021-03-19 17:47:04 +0800

    java spring boot

---
+++ src/main/java/com/example/demo/service/Hotel.java
@@ -0,0 +1,58 @@
+package com.example.demo.service;
+
+public class Hotel {
+
+    String province;
+    String city;
+    String wordone;
+    String wordtwo;
+    String finalword;
+
+    public Hotel(String province, String city, String wordone, String wordtwo, String finalword) {
+        this.province = province;
+        this.city = city;
+        this.wordone = wordone;
+        this.wordtwo = wordtwo;
+        this.finalword = finalword;
+    }
+
+    public String getFinalword() {
+        return finalword;
+    }
+
+    public void setFinalword(String finalword) {
+        this.finalword = finalword;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getWordone() {
+        return wordone;
+    }
+
+    public void setWordone(String wordone) {
+        this.wordone = wordone;
+    }
+
+    public String getWordtwo() {
+        return wordtwo;
+    }
+
+    public void setWordtwo(String wordtwo) {
+        this.wordtwo = wordtwo;
+    }
+}
